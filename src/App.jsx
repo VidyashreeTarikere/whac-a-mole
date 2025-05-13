3145b9c9db554e019b5e55f06137764bfd972161
VidyashreeTarikere <tvvidyashree@gmail.com>
2025-05-13 17:11:29 +0200

    background style added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Moles from "./Components/Moles";
+import Background from "./assets/Background.png";
 
 function App() {
   return (
     <>
-      <div className="body">
+      <div
+        className="body"
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          backgroundSize: "100% 100%",
+        }}
+      >
         <div className="title">Whac A Mole</div>
 
         <div className="moles">
